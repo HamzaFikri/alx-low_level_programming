67c0fc0e7b35db342551043fec12b336bb5086bc
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-17 06:23:28 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,6 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-
 /* betty style doc for function main goes there
  * main - Entry point
  *
@@ -11,6 +10,7 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	int n;
@@ -19,10 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is negative\n", n);
+		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%d is zero", n);
 	else
-		printf("%d is positive\n", n);
+		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
