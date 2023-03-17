6d863d0402d29583b50d29a91a9adf59569219c6
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-17 10:47:28 -0700

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,24 +3,26 @@
 /**
  * main - Entry point for 00 to 99
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
 {
-int i;
-int j;
+int x;
+int y;
 
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-int num1 = i * 10 + j;
-int num2 = j * 10 + i;
-if (num1 != num2)
-{
 putchar(i + '0');
 putchar(j + '0');
+if (i == 9 && j == 9)
+{
+break;
+}
+else
+{
 putchar(',');
 putchar(' ');
 }
