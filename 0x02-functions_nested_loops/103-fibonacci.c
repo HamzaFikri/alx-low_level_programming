ca19cbdd89dad628fdc87d0b25cee4ce281f681d
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-21 12:13:49 -0700

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-    int fibo[1000] = {1, 2};
-    int sum = 2;
-    int i = 2;
+int fibo [1000] = {1, 2};
+int sum = 2;
+int i = 2;
 
-    while (fibo[i-1] < 4000000)
-    {
-        fibo[i] = fibo[i-1] + fibo[i-2];
-        if (fibo[i] % 2 == 0)
-        {
-            sum += fibo[i];
-        }
-        i++;
-    }
-    printf("%d\n", sum);
-    return (0);
+while (fibo[i - 1] < 4000000)
+{
+fibo [i] = fibo [i - 1] + fibo [i - 2];
+if (fibo[i] % 2 == 0)
+{
+sum += fibo [i];
+}
+i++;
+}
+printf("%d\n", sum);
+return (0);
 }
