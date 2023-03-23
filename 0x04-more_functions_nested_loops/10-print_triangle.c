c9d960c1bd20e2a6e2445a9c3c77990b8abe50f0
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-23 07:41:22 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of square according parameter
+ *
+ * @size: sets the size of the triangle
+ *
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+		for (x = size; x > 0; x--)
+		{
+			for (y = 1; y <= size; y++)
+				if (y >= i)
+					_putchar('#');
+				else
+					_putchar(' ');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
