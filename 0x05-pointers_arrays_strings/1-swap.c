ec06afca89b2c6707ed96443114a728d010c5b96
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-27 03:48:18 -0700

    1. Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* swap_int -  swaps the values of two integers
+* @a: the first integer to be swapped.
+* @b: The second integer to be swapped.
+*/
+
+void swap_int(int *a, int *b)
+{
+int mix = *a;
+*a = *b;
+*b = mix;
+}
