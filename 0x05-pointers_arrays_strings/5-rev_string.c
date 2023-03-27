b7027a2208a72c235a5c360c6ac9b96ed10dd2ae
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-27 05:58:14 -0700

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,17 @@
 	void rev_string(char *s)
 	{
 		char rev = s[0];
-		int fcount = 0;
+		int fcounter = 0;
 		int i;
 
-		while (s[fcount] != '\0')
-			fcount++;
+		while (s[fcounter] != '\0')
+			fcounter++;
 
-		for (i = 0; i < fcount; i++)
+		for (i = 0; i < fcounter; i++)
 		{
-			fcount--;
+			fcounter--;
 			rev = s[i];
-			s[i] = s[fcount];
-			s[fcount] = rev;
+			s[i] = s[fcounter];
+			s[fcounter] = rev;
 		}
 	}
