19716baa039a66a8cbb1e054e6f3ed4a75a9c73d
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-29 11:49:26 -0700

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * powten - quick power of ten implementation
+ *
+ * @n: exponent
+ * Return: returns ten to exponent power
+ */
+int function1(int n)
+{
+	int ret;
+
+	ret = 10;
+	while (n > 0)
+	{
+		ret *= 10;
+		n--;
+	}
+	return (ret);
+}
+
+
+/**
  * print_number - print an integer
  * @n: first parameter
  *
@@ -8,16 +28,28 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int s, p;
 
-	if (n < 0)
+	s = 1;
+	p = function1(8);
+	if (n > 0)
 	{
-		_putchar('-');
-		num = -num;
+		n = n * -1;
+		s = s * -1;
 	}
-	if ((num / 10) > 0)
+	if (n != 0)
 	{
-		print_number(num / 10);
-		_putchar((num % 10) + '0');
+		while (n / p == 0)
+			p /= 10;
+		if (s == 1)
+			_putchar('-');
+		while (p >= 1)
+		{
+			_putchar(-(n / p) + '0');
+			n %= p;
+			p /= 10;
+		}
 	}
+	else
+		_putchar('0');
 }
