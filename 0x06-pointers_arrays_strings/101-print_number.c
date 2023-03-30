53d2004c3ff5d56f8dbf79084f5b6d9cd621d04b
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-03-30 04:47:27 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,55 +1,36 @@
 #include "main.h"
 
-/**
- * powten - quick power of ten implementation
- *
- * @n: exponent
- * Return: returns ten to exponent power
- */
-int function1(int n)
-{
-	int ret;
-
-	ret = 10;
-	while (n > 0)
-	{
-		ret *= 10;
-		n--;
-	}
-	return (ret);
-}
-
 
 /**
  * print_number - print an integer
  * @n: first parameter
- *
  * Return: 0
  */
 void print_number(int n)
 {
-	int s, p;
+	unsigned int i, j, cnt;
 
-	s = 1;
-	p = function1(8);
-	if (n > 0)
+	if (n < 0)
 	{
-		n = n * -1;
-		s = s * -1;
-	}
-	if (n != 0)
-	{
-		while (n / p == 0)
-			p /= 10;
-		if (s == 1)
-			_putchar('-');
-		while (p >= 1)
-		{
-			_putchar(-(n / p) + '0');
-			n %= p;
-			p /= 10;
-		}
+		_putchar(45);
+		i = n * -1;
 	}
 	else
-		_putchar('0');
+	{
+		i = n;
+	}
+
+	j = i;
+	cnt = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		cnt *= 10;
+	}
+
+	for (; cnt >= 1; cnt /= 10)
+	{
+		_putchar(((i / cnt) % 10) + 48);
+	}
 }
