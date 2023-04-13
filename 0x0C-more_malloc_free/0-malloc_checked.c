94b63eb0b72b2f0ea1ca67b56077e3c41aac3c6c
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-13 06:28:44 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocate memory using malloc.
@@ -13,6 +14,5 @@ void *malloc_checked(unsigned int b)
 	pntr = malloc(b);
 	if (pntr == NULL)
 		exit(98);
-	
 	return (pntr);
 }
