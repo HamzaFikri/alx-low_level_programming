1f4ed61b776542b63bd1eae200b4cb7b5c7d028d
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-13 06:24:48 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory using malloc.
+ * Description: If malloc fails, terminate process with status 98.
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to malloc'd memory space
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pntr;
+
+	pntr = malloc(b);
+	if (pntr == NULL)
+		exit(98);
+	return (pntr);
+}
