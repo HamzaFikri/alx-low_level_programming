00f3fbc1e0fceae2c155461b20979925cbb9d7e9
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-17 14:52:55 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,29 +11,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int i, lenght1, lenght2;
+	dog_t *d;
+	int lenght;
+	char *pntr;
 
-	lenght1 = lenght2 = i = 0;
-	while (name[lenght1++] != '\0')
-		;
-	while (owner[lenght2++] != '\0')
-		;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (name == 0 || owner == 0)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
 
-	new_dog->name = malloc(lenght1);
-	new_dog->owner = malloc(lenght2);
+	for (lenght = 1, pntr = name; *pntr; lenght++)
+		pntr++;
+	d->name = malloc(lenght);
+	if (d->name == 0)
+	{
+		free(d);
+		return (NULL);
+	}
 
-	do {
-		new_dog->name[i] = name[i];
-	} while (name[i++] != '\0');
-	i = 0;
-	do {
-		new_dog->owner[i] = owner[i];
-	} while (owner[i++] != '\0');
-	new_dog->age = age;
-	return (new_dog);
+	for (lenght = 1, pntr = owner; *pntr; lenght++)
+		pntr++;
+	d->owner = malloc(lenght);
+	if (d->owner == 0)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	for (lenght = 0; *name != 0; lenght++, name++)
+		d->name[lenght] = *name;
+	d->name[lenght] = 0;
+	for (lenght = 0; *owner != 0; lenght++)
+		d->owner[lenght] = *owner++;
+	d->owner[lenght] = 0;
+	d->age = age;
+
+	return (d);
 }
