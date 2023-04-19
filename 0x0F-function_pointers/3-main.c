0d800b4e608d418895fb941511da3e9e6e501a57
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-19 09:23:28 -0700

    Task 3 function main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, operat;
+	int (*res)(int, int);
+	char *get_op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = get_op_func(get_op);
+	operat = res(num1, num2);
+
+	printf("%d\n", operat);
+	return (0);
+}
