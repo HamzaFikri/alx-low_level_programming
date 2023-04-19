a20e8250fee2327e9fb9d0c4c26405c5dfdeb9a0
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-19 09:30:33 -0700

    Task 3 function main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,37 +8,23 @@
  * @argv: string of arguments in array
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num1, num2, operat;
-	int (*res)(int, int);
-	char *get_op;
+	int (*op_func)(int, int), num1, num2;
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	get_op = argv[2];
 
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(99);
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
 
-	res = get_op_func(get_op);
-	operat = res(num1, num2);
-
-	printf("%d\n", operat);
+	printf("%d\n", op_func(num1, num2));
 	return (0);
 }
