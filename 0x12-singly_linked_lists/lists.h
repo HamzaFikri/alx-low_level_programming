a441b5cbbd4e52ec25338e79b5d8bb67dab32f07
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-27 05:34:43 -0700

    Header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* data structure for this project */
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,12 +13,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /* Prototype */
