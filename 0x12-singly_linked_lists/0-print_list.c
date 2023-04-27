bc1d577b753948394b7115b72e373c26794d62b0
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-27 05:27:57 -0700

    Task 0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: struct
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *current;
+	size_t c;
+
+	c = 0;
+	current = h;
+	while (current != NULL)
+	{
+		printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		c++;
+	}
+	return (c);
+}
