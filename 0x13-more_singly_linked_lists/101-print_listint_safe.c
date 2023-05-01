d479f1cf01b71e252dcab9f31868379707ff0acb
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-05-01 06:38:21 -0700

    Task 12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,33 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Print a `listint_t` linked list including mem addresses
+ * print_listint_safe - Print `listint_t` linked list including mem addresses
  * @head: head of linked list
  * Description: Go through the list only once.
  * Return: number of nodes in list. If fails, exit with status 98.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
-		{
-			return (1 + print_listint_safe(head->next));
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
-		}
+	size_t x = 0, y;
+	const listint_t *temp_h = head, *check_next;
 
+	while (temp_h)
+	{
+		printf("[%p] %d\n", (void *)temp_h, temp_h->n);
+		x++;
+		temp_h = temp_h->next;
+		check_next = head;
+		y = 0;
+		while (y < x)
+		{
+			if (temp_h == check_next)
+			{
+				printf("-> [%p] %d\n", (void *)temp_h, temp_h->n);
+				return (x);
+			}
+			check_next = check_next->next;
+			y++;
+		}
 	}
-	return (0);
+	return (x);
 }
